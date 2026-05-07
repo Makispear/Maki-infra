704a6226685a1f971842f5835c94b269221ac10a
Maki Maki <makiaboabidamaki@gmail.com>
2026-05-07 01:06:17 -0400

    feat: integrate GitHub Actions with Terraform for automated secret syncing

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,13 +1,28 @@
-module "cdn" {
+module "s3" {
+  source      = "../../modules/s3"
+  bucket_name = "maki-frontend-dev"
+
+  cloudfront_arn = module.cloudfront.cloudfront_arn
+}
+
+module "cloudfront" {
   source = "../../modules/cloudfront"
 
   name           = "frontend-dev"
   s3_domain_name = module.s3.regional_domain_name
 }
 
-module "s3" {
-  source      = "../../modules/s3"
-  bucket_name = "maki-frontend-dev"
+resource "github_actions_secret" "cloudfront_id_secret" {
+  repository  = "Maki-Maki"
+  secret_name = "CLOUDFRONT_DIST_ID"
 
-  cloudfront_arn = module.cdn.cloudfront_arn
+  # 'value' is the modern replacement for 'plaintext_value'
+  value = module.cloudfront.distribution_id
+}
+
+# 4. Update GitHub Variables
+resource "github_actions_variable" "s3_bucket_var" {
+  repository    = "Maki-Maki"
+  variable_name = "S3_BUCKET_NAME"
+  value         = module.s3.bucket_name
 }
